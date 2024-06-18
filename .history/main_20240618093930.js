8e5f103767c041b5368b9c25e76cd44548e772fd
McBoatright <michaelboatright@pm.me>
2024-06-18 13:06:12 -0400

    added some style

---
+++ .history/main_20240618093930.js
@@ -0,0 +1,64 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const fs = require('fs');
+const path = require('path');
+
+ipcMain.handle('read-file', (event, filePath) => {
+    try {
+      return fs.readFileSync(filePath, 'utf-8');
+    } catch (error) {
+      console.error(`Failed to read file at ${filePath}:`, error);
+      return '[]';
+    }
+  });
+  
+ipcMain.handle('write-file', (event, filePath, data) => fs.writeFileSync(filePath, data));
+ipcMain.handle('join-path', (event, ...args) => path.join(...args));
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 600,
+    title: "The Ivy Lee Method",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, 
+    }
+  })
+
+  win.loadFile('index.html');
+
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.send('dirname', __dirname);
+  });
+
+
+  const instructionsWin = new BrowserWindow({
+    width: 400,
+    height: 300,
+    parent: win, 
+    modal: true, 
+    show: false, 
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, 
+    }
+  });
+
+  instructionsWin.loadFile('instructions.html'); 
+
+  instructionsWin.once('ready-to-show', () => {
+    instructionsWin.show(); 
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+})
---
+++ .history/renderer_20240618103101.js
@@ -0,0 +1,183 @@
+const { ipcRenderer } = require('electron');
+
+let currentPath;
+
+// Listen for the 'dirname' message
+ipcRenderer.on('dirname', (event, dirname) => {
+  currentPath = dirname;
+});
+
+const taskForm = document.getElementById('task-form');
+const taskInput = document.getElementById('task-input');
+const tasksDiv = document.getElementById('tasks');
+
+const noteForm = document.getElementById('note-form');
+const noteInput = document.getElementById('note-input');
+const notesDiv = document.getElementById('notes');
+
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newTask = taskInput.value;
+    taskInput.value = '';
+    addTask(newTask);
+});
+
+noteForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newNote = noteInput.value;
+    noteInput.value = '';
+    addNote(newNote);
+});
+
+async function addTask(task) {
+    const tasks = await getTasks();
+    if (tasks.length < 6) {
+        tasks.push(task);
+        if (currentPath) { // Check if currentPath is defined
+            const filePath = await ipcRenderer.invoke('join-path', currentPath, 'db.json');
+            await ipcRenderer.invoke('write-file', filePath, JSON.stringify(tasks));
+            displayTasks();
+        } else {
+            console.error('currentPath is undefined');
+        }
+    } else {
+        alert('You have reached the maximum number of tasks (6).');
+    }
+}
+
+async function getTasks() {
+    let tasks = [];
+    if (currentPath) { // Check if currentPath is defined
+        const filePath = await ipcRenderer.invoke('join-path', currentPath, 'db.json');
+        await ipcRenderer.invoke('read-file', filePath)
+            .then(tasksJson => {
+                if (tasksJson && tasksJson !== 'undefined' && tasksJson !== undefined) {
+                    try {
+                        tasks = JSON.parse(tasksJson);
+                    } catch (error) {
+                        console.error(`Failed to parse JSON from file at ${filePath}:`, error);
+                    }
+                }
+            })
+            .catch(error => {
+                console.error(`Failed to read file at ${filePath}:`, error);
+            });
+    } else {
+        console.error('currentPath is undefined');
+    }
+    return tasks;
+}
+
+async function displayTasks() {
+    while (!currentPath) {
+      // Wait for currentPath to be defined
+      await new Promise(resolve => setTimeout(resolve, 100));
+    }
+    const tasks = await getTasks();
+    tasksDiv.innerHTML = '';
+    tasks.forEach((task, index) => {
+      const taskElement = document.createElement('p');
+      taskElement.textContent = task;
+
+      const deleteButton = document.createElement('button');
+      deleteButton.className = 'delete-button'; 
+      deleteButton.addEventListener('click', () => deleteTask(index));
+      deleteButton.innerHTML = '<i class="fas fa-arrow-left"></i>'; // Use a solid left arrow icon
+      taskElement.appendChild(deleteButton);
+      tasksDiv.appendChild(taskElement);
+    });
+}
+
+async function deleteTask(index) {
+  const tasks = await getTasks();
+  tasks.splice(index, 1);
+  if (currentPath) { // Check if currentPath is defined
+    const filePath = await ipcRenderer.invoke('join-path', currentPath, 'db.json');
+    await ipcRenderer.invoke('write-file', filePath, JSON.stringify(tasks));
+    displayTasks();
+  } else {
+    console.error('currentPath is undefined');
+  }
+}
+
+async function addNote(note) {
+    const notes = await getNotes();
+    notes.push(note);
+    if (currentPath) {
+        const filePath = await ipcRenderer.invoke('join-path', currentPath, 'notes.json');
+        await ipcRenderer.invoke('write-file', filePath, JSON.stringify(notes));
+        displayNotes();
+    } else {
+        console.error('currentPath is undefined');
+    }
+}
+
+async function getNotes() {
+    let notes = [];
+    if (currentPath) {
+        const filePath = await ipcRenderer.invoke('join-path', currentPath, 'notes.json');
+        await ipcRenderer.invoke('read-file', filePath)
+            .then(notesJson => {
+                if (notesJson && notesJson !== 'undefined' && notesJson !== undefined) {
+                    try {
+                        notes = JSON.parse(notesJson);
+                    } catch (error) {
+                        console.error(`Failed to parse JSON from file at ${filePath}:`, error);
+                    }
+                }
+            })
+            .catch(error => {
+                console.error(`Failed to read file at ${filePath}:`, error);
+            });
+    } else {
+        console.error('currentPath is undefined');
+    }
+    return notes;
+}
+
+async function displayNotes() {
+    while (!currentPath) {
+      await new Promise(resolve => setTimeout(resolve, 100));
+    }
+    const notes = await getNotes();
+    notesDiv.innerHTML = '';
+    notes.forEach((note, index) => {
+      const noteElement = document.createElement('p');
+      noteElement.textContent = note;
+  
+      const deleteButton = document.createElement('button');
+      deleteButton.className = 'delete-button'; 
+      deleteButton.addEventListener('click', () => deleteNote(index));
+
+      noteElement.appendChild(deleteButton);
+      notesDiv.appendChild(noteElement);
+    });
+}
+
+async function deleteNote(index) {
+  const notes = await getNotes();
+  notes.splice(index, 1);
+  if (currentPath) {
+    const filePath = await ipcRenderer.invoke('join-path', currentPath, 'notes.json');
+    await ipcRenderer.invoke('write-file', filePath, JSON.stringify(notes));
+    displayNotes();
+  } else {
+    console.error('currentPath is undefined');
+  }
+}
+
+window.onload = () => {
+    const dateInput = document.getElementById('date-input');
+    
+    const savedDate = localStorage.getItem('date');
+    if (savedDate) {
+      dateInput.value = savedDate;
+    }
+  
+    dateInput.addEventListener('change', () => {
+      localStorage.setItem('date', dateInput.value);
+    });
+  
+    displayTasks();
+    displayNotes();
+  };
